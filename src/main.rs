c672edfe72824a26aecd89b36e17dec972c8fef4
Daniel Riegler <daniel.riegler87@googlemail.com>
2020-05-09 22:14:06 +0200

    test file works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+
+    let file = File::create("scene.ppm");
+    let mut file = match file {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    file.write_all(b"P3\n3 2\n255\n255 0 0 0 255 0 0 0 255\n255 255 0 255 255 255 0 0 0")?;
+
+    Ok(())
 }
